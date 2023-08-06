5341c89a230f67ce0a0717fc10a0c1797e0eb51f
Jaggar <18173108+GigiaJ@users.noreply.github.com>
2023-08-06 14:26:24 -0500

    we already have a batch file might as well run this command here

--- setup.bat
+++ setup.bat
@@ -1,2 +1,3 @@
 powershell -Command "Unblock-File .\installer.ps1"
 powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %~dp0 & echo \"yes\" | reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v \"ExecutionPolicy\" /d \"unrestricted\" & powershell .\installer.ps1'"
+"C:\Program Files (x86)\Steam\steam.exe " steam://AddNonSteamGame
