2cde89ac015f5fd600ebbb32570bed3ada8d3584
Jaggar <18173108+GigiaJ@users.noreply.github.com>
2023-08-06 15:33:16 -0500

    fuck it up I guess

--- installer.ps1
+++ installer.ps1
@@ -1,28 +1,24 @@
 if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
- Exit
+Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`" `"$($MyInvocation.MyCommand.UnboundArguments)`""
+Exit
 }
 
-$actionCommand = "C:\Program Files (x86)\Steam\steam.exe "
-$actionArgument = "steam://open/bigpicture"
-$action = New-ScheduledTaskAction -Execute $actionCommand -Argument $actionArgument
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 
 $taskName = "GamingMode"
+$actions = (New-ScheduledTaskAction -Execute "taskkill /f /im SteamController.exe"),
+(New-ScheduledTaskAction -Execute "taskkill /f /im explorer.exe"),
+(New-ScheduledTaskAction -Execute "C:\Program Files (x86)\Steam\steam.exe" -Argument "steam://open/bigpicture")
 $taskDescription = "Opens Steam in big picture mode"
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description $taskDescription -Settings $settings -RunLevel Highest -Force
+Register-ScheduledTask -Action $actions -Trigger $trigger -TaskName $taskName -Description $taskDescription -Settings $settings -RunLevel Highest -Force
 
 $taskName = "DesktopModeSteam"
 $taskDescription = "Opens Steam"
-$action = New-ScheduledTaskAction -Execute $actionCommand
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description $taskDescription -Settings $settings -RunLevel Highest -Force
+$actions = (New-ScheduledTaskAction -Execute "C:\Program Files (x86)\Steam\steam.exe"),
+(New-ScheduledTaskAction -Execute "C:\Program Files\SteamDeckTools\SteamController.exe")
 
-$actionCommand = "C:\Program Files\SteamDeckTools\SteamController.exe"
-$taskName = "DesktopModeSteamController"
-$taskDescription = "Opens the SteamController program"
-$action = New-ScheduledTaskAction -Execute $actionCommand
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description $taskDescription -Settings $settings -RunLevel Highest -Force
+Register-ScheduledTask -Action $actions -Trigger $trigger -TaskName $taskName -Description $taskDescription -Settings $settings -RunLevel Highest -Force
 
 .\iexpress.bat .\LaunchDesktop.bat LaunchDesktop.exe
 
