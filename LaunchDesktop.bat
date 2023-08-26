ce89c70685de96ef8ea0f316fc0a408ca5b95ddc
Jaggar <18173108+GigiaJ@users.noreply.github.com>
2023-08-26 17:31:24 -0400

    Update LaunchDesktop.bat
    I think this should fix non-activated Windows versions from having their Steam crash in gaming mode and not have an easy way to reopen it

--- LaunchDesktop.bat
+++ LaunchDesktop.bat
@@ -1,6 +1,6 @@
 echo "yes" | REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d explorer.exe
-echo "yes" | REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell
-echo "yes" | REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d ""
 taskkill /f /im Steam.exe
 explorer.exe
+echo "yes" | REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell
+echo "yes" | REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "\"C:\Program Files (x86)\Steam\steam.exe\" steam://open/bigpicture"
 schtasks /run /tn "DesktopMode"
